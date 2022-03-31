5cd7164a99da0f84608e09821bb80417febb6ea9
Willy Tan <willy.tan@pepro.io>
2022-03-31 16:11:50 +0200

    add feature search file

--- qchecker/qchecker/ODR_Checker.py
+++ qchecker/qchecker/ODR_Checker.py
@@ -1,46 +1,55 @@
 
 import os
-import fnmatch
 import sys
+import fnmatch
 import traceback
-from xml.etree import ElementTree as ET
 import xmlschema
+import pathlib
 import csv
-
-xsd_file = 'OpenDRIVE_1.5M.xsd'
-xml_file = '/data/inputs/AB_Oval.xodr'
-
-output = []
-my_schema = xmlschema.XMLSchema(xsd_file)
-print('If ', xml_file, ' is valid: ', my_schema.is_valid(xml_file))
-
-result = my_schema.is_valid(xml_file)
-
-output1 = 'If ' + xml_file + ' is valid against' + xsd_file + '?' , result 
-
-output.append(output1)
-
-odr_as_dict = my_schema.to_dict(xml_file)
-
-my_schema.validate(xml_file)
-
-xml_tree = ET.parse(xml_file)
-root = xml_tree.getroot()
+from xml.etree import ElementTree as ET
 
 
+def main():
+    # file paths
+    xsd_file = "OpenDRIVE_1.5M.xsd"
+    xml_directory = r"..\data\inputs"
+    xml_extension = r".xodr"
+    xml_file = ""
 
-for header in root.iter('header'):
-  
-    print(header.attrib.get('date'))
-    output2 = 'Date and Time Created : ' , header.attrib.get('date')
-    output.append(output2)
+    # find the file name
+    xml_file = [os.path.join(xml_directory, _) for _ in os.listdir(xml_directory) if _.endswith(xml_extension)]
+    print(xml_file[0])
 
-print(output)
+    output = []
+    my_schema = xmlschema.XMLSchema(xsd_file)
+    print('If ', xml_file[0], ' is valid: ', my_schema.is_valid(xml_file[0]))
 
-with open('/data/outputs/CheckerLog.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
+    result = my_schema.is_valid(xml_file[0])
 
-    writer.writerow(output1)
-    writer.writerow(output2)
+    output1 = 'If ' + xml_file[0] + ' is valid against' + xsd_file + '?' , result
 
-    f.close()
+    output.append(output1)
+
+
+    xml_tree = ET.parse(xml_file[0])
+    root = xml_tree.getroot()
+
+    for header in root.iter('header'):
+    
+        print(header.attrib.get('date'))
+        output2 = 'Date and Time Created : ' , header.attrib.get('date')
+        output.append(output2)
+
+    print(output)
+
+    with open('/data/outputs/CheckerLog.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+
+        writer.writerow(output1)
+        writer.writerow(output2)
+
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
