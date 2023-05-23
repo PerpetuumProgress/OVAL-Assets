eae055e0bb7c907b0055333eb1282447ec2a1004
shariquetaj <sharique.taj@pepro.io>
2023-05-23 11:21:23 +0200

    Dockerfile, entrypoint updated, docker image pushed

--- algorithms/carla/entrypoint.sh
+++ algorithms/carla/entrypoint.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 ## Run the carla server
-su -c './carla/CarlaUE4.sh -RenderOffScreen' carla
+su -c './home/carla/CarlaUE4.sh -RenderOffScreen' carla &
+
+while ! nc -z 127.0.0.1 2000; do
+        sleep 20
+done
+
+## Prepare the scenario
+file=$(find data/inputs -type f -name "0" -print -quit)
+#cp "$file" data/inputs
+
+## Connect to the carla server and run the scenario
+cd /home/scenario_runner-0.9.13/
+python3 scenario_runner.py --openscenario /data/inputs/0 --reloadWorld --trafficManagerPort 8005 --json
+mv /home/scenario_runner-0.9.13/*.json /data/outputs/
 
 
-## In a new Terminal connect to the carla server (Not working)
-xterm -e 'cd /home/scenario_runner-0.9.13/'
-python3 scenario_runner.py --openscenario srunner/examples/OscControllerExample.xosc
+## Testing purpose
+# python3 scenario_runner.py --openscenario srunner/examples/OscControllerExample.xosc --reloadWorld --trafficManagerPort 8005 --json
+# cd /
+# python3 record_log.py
 
 
-# file=$(find data/inputs -type f -name "0" -print -quit)
-# cp "$file" data/inputs
+
+
--- algorithms/carla/run_carla_container.sh
+++ algorithms/carla/run_carla_container.sh
@@ -1,3 +1,3 @@
-docker run -it --rm --privileged -u root --gpus all --net=host -e DISPLAY=$DISPLAY --volume /temp/.X11-unix:/temp/.X11-unix carla:0.9.13_test /bin/bash
-
+#docker run -it --rm --privileged -u root --gpus all --net=host -e DISPLAY=$DISPLAY --volume /temp/.X11-unix:/temp/.X11-unix -v /outputs:/data/outputs carla:0.9.13_test /bin/bash
+docker run -it --rm --privileged -u root --gpus all --net=host -e DISPLAY=$DISPLAY --volume /temp/.X11-unix:/temp/.X11-unix -v $(pwd)/data/outputs:/data/outputs -v $(pwd)/data/inputs/0:/data/inputs/0 carla:0.9.13_test ./entrypoint.sh
 
