3609d17abd84cfcd27f292ae08db0b22c7efc57b
w-pepro <willy.tan@pepro.io>
2024-02-26 14:03:02 +0100

    change entrypoint.sh und add __main__ to train_and_predict.py

--- algorithms/Pepro_Objekterkennung_Demo/pytorch/entrypoint.sh
+++ algorithms/Pepro_Objekterkennung_Demo/pytorch/entrypoint.sh
@@ -5,6 +5,6 @@ cd /data/inputs; tar -xvf $(find /data/inputs/ -name "0")
 echo "Find local_data_path ..."
 echo $(dirname $(find /data/inputs/ -type d -name "cifar-10-batches-py"))
 
-python train_and_predict.py $(dirname $(find /data/inputs/ -type d -name "cifar-10-batches-py"))
+python $ALGO/train_and_predict.py $(dirname $(find /data/inputs/ -type d -name "cifar-10-batches-py"))
 
 
