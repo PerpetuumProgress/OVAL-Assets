623b69ef99708fba57469b782f5ecbed3f2e7653
w-pepro <willy.tan@pepro.io>
2024-02-26 12:01:37 +0100

    change extrynpoint.sh

--- algorithms/Pepro_Objekterkennung_Demo/pytorch/entrypoint.sh
+++ algorithms/Pepro_Objekterkennung_Demo/pytorch/entrypoint.sh
@@ -3,8 +3,8 @@
 cd /data/inputs; tar -xvf $(find /data/inputs/ -name "0")
 
 echo "Find local_data_path ..."
-echo $(dirname $(find ./data/inputs/ -type d -name "cifar-10-batches-py"))
+echo $(dirname $(find /data/inputs/ -type d -name "cifar-10-batches-py"))
 
-python train_and_predict.py $(dirname $(find ./data/inputs/ -type d -name "cifar-10-batches-py"))
+python train_and_predict.py $(dirname $(find /data/inputs/ -type d -name "cifar-10-batches-py"))
 
 
