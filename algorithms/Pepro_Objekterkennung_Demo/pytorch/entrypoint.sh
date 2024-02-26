b3ac9b5e5684df1dcca9d723f22fe23f910af2a0
w-pepro <willy.tan@pepro.io>
2024-02-26 15:54:36 +0100

    change entrypoint.sh

--- algorithms/Pepro_Objekterkennung_Demo/pytorch/entrypoint.sh
+++ algorithms/Pepro_Objekterkennung_Demo/pytorch/entrypoint.sh
@@ -5,6 +5,8 @@ cd /data/inputs; tar -xvf $(find /data/inputs/ -name "0")
 echo "Find local_data_path ..."
 echo $(dirname $(find /data/inputs/ -type d -name "cifar-10-batches-py"))
 
+echo $ALGO
+
 python $ALGO $(dirname $(find /data/inputs/ -type d -name "cifar-10-batches-py"))
 
 
