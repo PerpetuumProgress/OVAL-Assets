86c8d67055e51f434b751fa6ed6dcfe3d957e488
w-pepro <willy.tan@pepro.io>
2024-06-04 17:02:27 +0200

    add files to kivi demo

--- algorithms/Kivi_Demo/entrypoint.sh
+++ algorithms/Kivi_Demo/entrypoint.sh
@@ -3,13 +3,19 @@
 cd /data/inputs; tar -xvf $(find /data/inputs/ -name "0")
 
 echo "Find local_data_path ..."
-echo $(dirname $(find /data/inputs/ -type d -name "KIVI_data"))
+local_data_path=$(find /data/inputs/ -type d -name "KIVI_data" | head -n 1)
 
-echo "Copy Data to Local Path"
-cp $(dirname $(find /data/inputs/ -type d -name "KIVI_data")) /usr/src/custom_data
+if [ -d "$local_data_path" ]; then
+    echo "Found local_data_path: $local_data_path"
+    echo "Copy Data to Local Path"
+    cp -r "$local_data_path/"* /usr/src/custom_data/
+else
+    echo "KIVI_data folder not found!"
+    exit 1
+fi
 
-echo "Tree /data/:"
-ls -R /data/
+cd ../..
 
 echo "Start algorithm ..."
-python3 ./val.py --weights trained_network.pt --data test.yaml 
+python3 ./usr/src/app/val.py --weights ./usr/src/app/trained_network.pt --data ./usr/src/app/data/test.yaml --project ./data/outputs
+echo "Stop algorithm ..."
