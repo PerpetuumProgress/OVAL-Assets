774378a7cd4dc6038e01c2626d9bca0a842d5c6e
w-pepro <willy.tan@pepro.io>
2024-06-04 18:42:24 +0200

    add timeout to entrypoint.sh from kivi demi

--- algorithms/Kivi_Demo/entrypoint.sh
+++ algorithms/Kivi_Demo/entrypoint.sh
@@ -17,5 +17,5 @@ fi
 cd ../..
 
 echo "Start algorithm ..."
-python3 ./usr/src/app/val.py --weights ./usr/src/app/trained_network.pt --data ./usr/src/app/data/test.yaml --project ./data/outputs
+timeout 30 python3 ./usr/src/app/val.py --weights ./usr/src/app/trained_network.pt --data ./usr/src/app/data/test.yaml --project ./data/outputs
 echo "Stop algorithm ..."
