2f5569927f5dc2862450b7ed090494f131a29442
w-pepro <willy.tan@pepro.io>
2024-06-06 14:35:54 +0200

    change entrypointdata.sh

--- algorithms/Kivi_Demo/entrypointdata.sh
+++ algorithms/Kivi_Demo/entrypointdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Start algorithm ..."
-python3 ./usr/src/app/val.py --weights ./usr/src/app/trained_network.pt --data ./usr/src/app/data/test.yaml --project ./data/outputs/
+python3 ./val.py --weights ./trained_network.pt --data ./data/test.yaml --project ../../../data/outputs/
 echo "Stop algorithm ..."
