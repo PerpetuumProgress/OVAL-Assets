f5b3ff1bd5bf70b13a417e63d3ad7aede1cf4892
w-pepro <willy.tan@pepro.io>
2024-06-10 22:51:40 +0200

    change kivi demo

--- algorithms/Kivi_Demo/entrypointdata.sh
+++ algorithms/Kivi_Demo/entrypointdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Start algorithm ..."
-cp ../custom_data/runs ../../data/outputs
+cp ../custom_data/runs/result.zip ../../../data/outputs/result.zip
 python3 ./val.py --weights ./trained_network.pt --data ./data/test.yaml --project ../../../data/outputs/
 echo "Stop algorithm ..."
